c89b07e2cdb758902967544f3be9d8bdbbf5fa91
jaimealba <jaimealba@outlook.es>
2023-10-14 14:23:10 +0200

    14/10/2023 14:23
    Push para ver si el GitKraken de mier** da problemas o no

---
+++ TrabajoAccDat1Equ4/src/main/java/com/mycompany/trabajoaccdat1equ4/TrabajoAccDat1Equ4.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.trabajoaccdat1equ4;
+
+/**
+ *
+ * @author pokem
+ */
+public class TrabajoAccDat1Equ4 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
