6a1fa307713c21751eb4a34677a375e241872bb4
Jaime Alba Ruiz <jalbar03@educastillalamancha.es>
2023-10-18 00:37:31 +0200

    18/10/2023 00:37
    Conflictos resueltos. Código conjunto que incluye el último push de Bacho (mover directorios y su contenido) y el mío. Todo funcional.

--- TrabajoAccDat1Equ4/src/main/java/com/mycompany/trabajoaccdat1equ4/TrabajoAccDat1Equ4.java
+++ TrabajoAccDat1Equ4/src/main/java/com/mycompany/trabajoaccdat1equ4/TrabajoAccDat1Equ4.java
@@ -76,9 +76,11 @@ public class TrabajoAccDat1Equ4 {
         bestiario.delete();
         
         
-        Metodos.reescrituraID("tierra", ".\\Zona 1\\Grupo 1-3.dat");
-        Metodos.convertirGrupoAXml(".\\Zona 1\\Grupo 1-3.dat", "Grupo 1-3.xml");
-        Metodos.moverGrupo(".\\Grupo 1-3.xml", ".\\Zona 1\\Grupo 1-3.xml");
+        //Metodos.reescrituraID("tierra", ".\\Zona 3\\Grupo 3-3.dat");
+        Metodos.modoFacil(".\\Zona 3\\Grupo 3-3.dat", "fuego");
+        Metodos.convertirGrupoAXml(".\\Zona 3\\Grupo 3-3.dat", ".\\Zona 3\\Grupo 3-3.xml");
+        Metodos.modificarXml(".\\Zona 3\\Grupo 3-3.xml", "Hydrobabosa", "Tengo sueño cojones");
+
         
     }
     
