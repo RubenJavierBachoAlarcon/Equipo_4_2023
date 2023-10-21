accc00c9770eaa7e9949c4bcca7aff379133ca2f
Jaime Alba Ruiz <jalbar03@educastillalamancha.es>
2023-10-21 05:04:54 +0200

    21/10/2023 05:04
    Implementación de los métodos para añadir y borrar nodos XML.

--- TrabajoAccDat1Equ4/src/main/java/com/mycompany/trabajoaccdat1equ4/Metodos.java
+++ TrabajoAccDat1Equ4/src/main/java/com/mycompany/trabajoaccdat1equ4/Metodos.java
@@ -1054,7 +1054,7 @@ public class Metodos {
                     // Se compara que el elemento texto contenido por el nodo
                     // que está siendo evaluado coincida con el texto contenido
                     // por la String que se ha pasado por parámetro
-                    if (nodo.getTextContent().equals(textoNodo)) {
+                    if (nodo.getTextContent().equalsIgnoreCase(textoNodo)) {
                         nodo.setTextContent(textoNuevo);
                     }
                 }
@@ -1536,5 +1536,323 @@ public class Metodos {
         
         File bestiario = new File(".\\enemigos.dat");
         bestiario.delete();
+    }
+    
+    
+    
+    /**
+     * Método al que se le pasa por parámetro una String que contiene el nombre
+     * del monstruo que se está evaluando y devuelve el grito o reacción que
+     * emitirá dicho monstruo cuando se entre en batalla con él.
+     * @param enemigo String que contiene el nombre del enemigo que se está
+     * evaluando
+     * @return String que contiene el grito de guerra emitido por un monstruo
+     * al enfrentarlo en combate
+     */
+    public static String obtenerGritoGuerra(String enemigo) {
+        String grito = null;
+        
+        switch (enemigo) {
+            case "Pyrobabosa" -> {
+                grito = new String("¡Grrrblrblrblr!");
+            }
+            case "Geobabosa" -> {
+                grito = new String("¡GRBL BLGR!");
+            }
+            case "Hydrobabosa" -> {
+                grito = new String("¡Contrariando la creencia popular, algunas babosas sabemos hablar!");
+            }
+            case "Pyrodragarto" -> {
+                grito = new String("¡Hisssssssssss!");
+            }
+            case "Geodragarto" -> {
+                grito = new String("¡Hiss-hiss, hiss-hiss!");
+            }
+            case "Hydrodragarto" -> {
+                grito = new String("*Parpadea, visiblemente confuso*");
+            }
+            case "Pyroesqueleto" -> {
+                grito = new String("¡Usaré tu carne para recubrir mis huesos!");
+            }
+            case "Geoesqueleto" -> {
+                grito = new String("¡Me haré un traje con tu pellejo!");
+            }
+            case "Hydroesqueleto" -> {
+                grito = new String("Oye, ¿haces algo esta noche? *Sonidos de huesos*");
+            }
+            case "Pyrocaballero" -> {
+                grito = new String("¡En guardia!");
+            }
+            case "Geocaballero" -> {
+                grito = new String("¡Prepárate a morir!");
+            }
+            case "Hydrocaballero" -> {
+                grito = new String("Otra vez a pelear... ¿cuánto falta para el viernes?");
+            }
+            case "Pyrocapitán" -> {
+                grito = new String("¡¡TROPAS!! ¡¡EN FORMACIÓN!!");
+            }
+            case "Geocapitán" -> {
+                grito = new String("¡¡RESISTID, SOLDADOS!!");
+            }
+            case "Hydrocapitán" -> {
+                grito = new String("Otra vez trabajando en mi día libre...");
+            }
+            case "Pyrodiablillo" -> {
+                grito = new String("¡Ñijiji! ¡Prepárate a sufrir!");
+            }
+            case "Geodiablillo" -> {
+                grito = new String("¡Ñujuju! ¡Se avecina tu suplicio!");
+            }
+            case "Hydrodiablillo" -> {
+                grito = new String("Yo no me río. ¿Tienes un euro para el bus?");
+            }
+            case "Pyrobestia" -> {
+                grito = new String("¡¡¡GROAAAAAAAAAAAAAAAR!!!");
+            }
+            case "Geobestia" -> {
+                grito = new String("Grrrrrr... grrrhrhgrrrrhrhgrrrrhg...");
+            }
+            case "Hydrobestia" -> {
+                grito = new String("*Puedes escucharla roncar*");
+            }
+            case "Pyrodemonio" -> {
+                grito = new String("Tu perdición... está cerca...");
+            }
+            case "Geodemonio" -> {
+                grito = new String("Aprovecha estos minutos. Para ti, serán los últimos.");
+            }
+            case "Hydrodemonio" -> {
+                grito = new String("Lo cierto es que me habría gustado estudiar Artes.");
+            }
+            case "Wyvern demente" -> {
+                grito = new String("*Lanza una llamarada al aire*");
+            }
+        }
+        
+        return grito;
+    }
+    
+    
+    
+    /**
+     * Método al que se le pasa por parámetro una String que contiene el nombre
+     * del monstruo que se está evaluando y devuelve la impresión que se
+     * tiene de él.
+     * @param enemigo String que contiene el nombre del enemigo que se está
+     * evaluando
+     * @return String que contiene las impresiones que se obtienen del monstruo
+     * evaluado
+     */
+    public static String obtenerEsLindo(String enemigo) {
+        String grito = null;
+        
+        switch (enemigo) {
+            case "Pyrobabosa" -> {
+                grito = new String("Te dan ganas de darle un pyrobesito.");
+            }
+            case "Geobabosa" -> {
+                grito = new String("Pues... ni fu, ni fa.");
+            }
+            case "Hydrobabosa" -> {
+                grito = new String("Te dan ganas de que no hable tanto.");
+            }
+            case "Pyrodragarto" -> {
+                grito = new String("Se lo ve escamosito. Es lindo.");
+            }
+            case "Geodragarto" -> {
+                grito = new String("También es escamosito, pero un poco menos lindo.");
+            }
+            case "Hydrodragarto" -> {
+                grito = new String("Te dan ganas de acariciarlo.");
+            }
+            case "Pyroesqueleto" -> {
+                grito = new String("Está un poco en los huesos.");
+            }
+            case "Geoesqueleto" -> {
+                grito = new String("No es feo, sólo es complicado de mirar.");
+            }
+            case "Hydroesqueleto" -> {
+                grito = new String("Mono, lo que se dice mono, no es.");
+            }
+            case "Pyrocaballero" -> {
+                grito = new String("Demasiado mazado. No es tu tipo.");
+            }
+            case "Geocaballero" -> {
+                grito = new String("Es normalucho, pero tirando a guapete.");
+            }
+            case "Hydrocaballero" -> {
+                grito = new String("El ser tan quejica le quita atractivo.");
+            }
+            case "Pyrocapitán" -> {
+                grito = new String("Lo cierto es que es todo un galán.");
+            }
+            case "Geocapitán" -> {
+                grito = new String("Le notas una musculatura rocosa.");
+            }
+            case "Hydrocapitán" -> {
+                grito = new String("Además de apuesto, parece adinerado.");
+            }
+            case "Pyrodiablillo" -> {
+                grito = new String("Está bastante raquítico.");
+            }
+            case "Geodiablillo" -> {
+                grito = new String("Es feo. Feo con ganas. Feo con saña y avaricia.");
+            }
+            case "Hydrodiablillo" -> {
+                grito = new String("Tiene cara como de haber estado chupando limones.");
+            }
+            case "Pyrobestia" -> {
+                grito = new String("Está bastante enfadada. Ganas de acariciarla no te dan.");
+            }
+            case "Geobestia" -> {
+                grito = new String("Parece una tortuga grande. Sientes el deseo de darle lechuga.");
+            }
+            case "Hydrobestia" -> {
+                grito = new String("La ves tan dormidita que te dan ganas de acurrucarte con ella.");
+            }
+            case "Pyrodemonio" -> {
+                grito = new String("Lo cierto es que es bastante atractivo.");
+            }
+            case "Geodemonio" -> {
+                grito = new String("No está mal, pero necesita una ducha.");
+            }
+            case "Hydrodemonio" -> {
+                grito = new String("Si se cortase esas greñas, sería guapillo.");
+            }
+            case "Wyvern demente" -> {
+                grito = new String("No es demasiado monete. De hecho, da canguelo.");
+            }
+        }
+        
+        return grito;
+    }
+    
+    
+    
+    /**
+     * Método que permite añadir nodos XML, dependiendo de una determinada
+     * estructura de archivos XML, a un archivo cuya ruta se le pase por
+     * parámetro. 
+     * El nombre del nodo que se desea añadir también deberá pasarse por
+     * parámetro; únicamente hay implementación para los nodos "gritoguerra"
+     * y "eslindo".
+     * @param rutaArchivo String que contiene la ruta del archivo XML al que
+     * se añadirán los nodos.
+     * @param nombreNodo String que contiene el nombre del nodo a añadir;
+     * las posibilidades por el momento son "gritoguerra" y "eslindo".
+     */
+    public static void añadirNodoXml(String rutaArchivo, String nombreNodo) {
+        try {
+           File archivo = new File(rutaArchivo);
+           // Parseamos el archivo xml a utilizar
+           DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+           DocumentBuilder builder = factory.newDocumentBuilder();
+           Document documento = builder.parse(archivo);
+           
+           // Le aplicamos normalización
+           documento.getDocumentElement().normalize();
+           
+           // Recuperamos la lista de nodos etiquetados como "Enemigo"
+           // y la recorremos
+           NodeList listaNodos = documento.getElementsByTagName("Enemigo");
+           
+           for (int i=0; i<listaNodos.getLength(); i++) {
+               Node nodo = listaNodos.item(i);
+               // Con ésto podemos iterar entre los distintos nodos "Enemigo".
+               
+               
+               // Si lo que se están recorriendo son nodos, se les hace un
+               // casting a objeto de la clase Elemento para poder tratarlos
+               // como tal (como cuando escribimos ficheros XML con DOM)
+               if (nodo.getNodeType() == Node.ELEMENT_NODE) {
+                   Element elemento = (Element) nodo;
+                   
+                   // Tras ello, se verifica si el nodo cuyo nombre pasado por
+                   // parámetro existe; si no existe, se crea y añade, y si 
+                   // existe, no se aplican cambios 
+                   NodeList listaNodosParam = elemento.getElementsByTagName(nombreNodo);
+                   if (listaNodosParam.getLength() == 0) {
+                       // Se guarda en una String el texto que contiene
+                       // el nodo etiquetado como "nombre"
+                       String nombreMonstruo = elemento.getElementsByTagName("nombre").item(0).getTextContent();
+                       
+
+                       // Obtenemos el contenido del nodo a añadir
+                       // según el nombre del monstruo
+                       String textoNodo = null;
+                       if (nombreNodo.equals("gritoguerra")) {
+                           textoNodo=Metodos.obtenerGritoGuerra(nombreMonstruo);
+                       } else if (nombreNodo.equals("eslindo")) {
+                           textoNodo=Metodos.obtenerEsLindo(nombreMonstruo);
+                       }
+                       
+                       
+                       // Utilizamos el método crearNodo para crear
+                       // un nodo cuyo contenido es texto. Este nodo pasa
+                       // a colgar del elemento que se indique por parámetro,
+                       // en este caso los nodos Enemigo por los que se
+                       // estaba iterando
+                       Metodos.crearNodo(nombreNodo, textoNodo, documento, elemento);
+                       
+                   }
+               }
+           }
+           
+           // Y se crea el archivo xml
+           Metodos.generarXml(documento, rutaArchivo);
+           
+           
+           
+       } catch (Exception e) {
+           System.out.println("Excepción al añadir nodos xml");
+       }
+    }
+    
+    
+    
+    /**
+     * Método que permite eliminar nodos XML en un archivo cuya ruta se pase
+     * por parámetro. Los nodos que se eliminarán serán aquellos cuya etiqueta
+     * coincida con una String pasada por parámetro.
+     * @param rutaArchivo String que contiene la ruta del archivo XML del que
+     * se eliminarán los nodos.
+     * @param nombreNodo String que contiene la etiqueta del nodo a eliminar;
+     * las posibilidades por el momento son "gritoguerra" y "eslindo".
+     */
+    public static void eliminarNodoXml(String rutaArchivo, String nombreNodo) {
+        try {
+            // Se empieza parseando y normalizando el archivo cuya ruta
+            // se pasa por parámetro
+            File archivo = new File(rutaArchivo);
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document documento = builder.parse(archivo);
+            documento.normalize();
+            
+            
+            // Recogemos en una lista de nodos todos aquells nodos cuya
+            // etiqueta coincida con la String que se pasa por parámetro
+            // ("gritoguerra" o "eslindo")
+            NodeList listaNodos = documento.getElementsByTagName(nombreNodo);
+            
+            
+            // Recorremos la lista de nodos obtenida y, en cada uno de los nodos,
+            // accedemos al padre; desde éste, indicamos que se elimine el nodo
+            // hijo indicado, que es el mismo que se está recorriendo
+            // Conseguimos de esta manera eliminar de manera efectiva los mismos
+            // nodos que estamos recorriendo, es decir, aquellos cuya etiqueta
+            // coincide con la String pasada por parámetro
+            for (int i=0; i<listaNodos.getLength(); i++) {
+                Node nodo = listaNodos.item(i);
+                nodo.getParentNode().removeChild(nodo);
+            }
+            
+            // Y regeneramos el XML
+            Metodos.generarXml(documento, rutaArchivo);
+            
+        } catch (Exception e) {
+            System.out.println("Excepción al eliminar nodos xml");
+        }
     }
 }
